9a5d0116941230295bafaded584d4f702c5c332e
zach <zacharybeattiefl@gmail.com>
2024-07-07 14:18:55 -0400

    finished randomguesser

--- NumberGuesser/script.js
+++ NumberGuesser/script.js
@@ -4,3 +4,20 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (humanGuess, computerGuess, target) => {
+  const humanDifference = Math.abs(target - humanGuess);
+  const computerDifference = Math.abs(target - computerGuess);
+    return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
